92fa2ce3cb1b55933f6ccfb50613a0fc69d2f13a
Gabriel Sales <gabriel.guelsi@gmail.com>
2026-04-01 16:15:30 +0100

    fix 405 on student update: accept both PUT and PATCH

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
     Route::get('students',                               [StudentController::class, 'index'])->name('students.index');
     Route::get('students/{student}',                     [StudentController::class, 'show'])->name('students.show');
     Route::get('students/{student}/edit',                [StudentController::class, 'edit'])->name('students.edit');
-    Route::put('students/{student}',                     [StudentController::class, 'update'])->name('students.update');
+    Route::match(['PUT','PATCH'], 'students/{student}',  [StudentController::class, 'update'])->name('students.update');
     Route::patch('students/{student}/reassign',          [StudentController::class, 'reassign'])->name('students.reassign');
     Route::post('students/bulk-reassign',                [StudentController::class, 'bulkReassign'])->name('students.bulkReassign');
     Route::patch('students/{student}/gift-received',     [StudentController::class, 'markGiftReceived'])->name('students.markGiftReceived');

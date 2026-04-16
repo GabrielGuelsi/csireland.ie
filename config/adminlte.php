604bef1f574ebda9c2d67d38a04ac07970889255
Gabriel Sales <gabriel.guelsi@gmail.com>
2026-04-16 17:35:42 +0100

    adding CI logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'use_ico_only' => false,
+    'use_ico_only' => true,
     'use_full_favicon' => false,
 
     /*
@@ -64,11 +64,11 @@ return [
     */
 
     'logo' => '<b style="color:#F26522">CI</b><span style="color:#fff">CORE</span>',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img' => 'img/ci-logo.png',
+    'logo_img_class' => 'brand-image elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img_alt' => 'CICORE',
 
     /*
     |--------------------------------------------------------------------------
